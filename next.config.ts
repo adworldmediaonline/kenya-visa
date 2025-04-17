d0161f51d3b323725e8c41144f21764ee434b3ee
fastestsunil <sunil141292@gmail.com>
2025-04-17 15:33:53 +0530

    feat: Update image configuration in next.config.ts to use remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["placehold.co"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
   },
 };
 
